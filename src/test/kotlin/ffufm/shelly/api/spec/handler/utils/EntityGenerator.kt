d5cdd154ecc326b53ff1f8e8541a33f6da1c9b0e
khyle24 <43634021+khyle24@users.noreply.github.com>
2022-01-28 14:02:07 +0800

    initialTestingforTodo

--- src/test/kotlin/ffufm/shelly/api/spec/handler/utils/EntityGenerator.kt
+++ src/test/kotlin/ffufm/shelly/api/spec/handler/utils/EntityGenerator.kt
@@ -1,10 +1,16 @@
 package ffufm.shelly.api.spec.handler.utils
 
+import ffufm.shelly.api.spec.dbo.todo.TodoToDo
 import ffufm.shelly.api.spec.dbo.user.UserUser
+import ffufm.shelly.api.spec.dbo.user.UserUserDTO
 
 object EntityGenerator {
-    fun createUser(): UserUser = UserUser(
+    fun createUser(): UserUserDTO = UserUserDTO(
         name = "Brandon",
         email = "brandon@gmail.com"
+    )
+    fun createTodo(): TodoToDo= TodoToDo(
+        description = "Assignment 1",
+        status = "Pending"
     )
 }
