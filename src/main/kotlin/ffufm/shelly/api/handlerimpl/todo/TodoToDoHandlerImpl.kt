f8d54f36d2f523d6168ed6e771e6871a8de269b6
Jonathan Juayong <jonathan.juayong@gmail.com>
2022-01-28 11:33:37 +0800

    implement CRUD methods

--- src/main/kotlin/ffufm/shelly/api/handlerimpl/todo/TodoToDoHandlerImpl.kt
+++ src/main/kotlin/ffufm/shelly/api/handlerimpl/todo/TodoToDoHandlerImpl.kt
@@ -2,15 +2,21 @@ package ffufm.shelly.api.handlerimpl.todo
 
 import de.ffuf.pass.common.handlers.PassDatabaseHandler
 import de.ffuf.pass.common.utilities.extensions.orElseThrow404
+import de.ffuf.pass.common.utilities.extensions.toDtos
 import ffufm.shelly.api.repositories.todo.TodoToDoRepository
 import ffufm.shelly.api.spec.dbo.todo.TodoToDo
+import ffufm.shelly.api.spec.dbo.todo.TodoToDoDTO
 import ffufm.shelly.api.spec.handler.todo.TodoToDoDatabaseHandler
+import ffufm.shelly.api.utils.Statuses
 import kotlin.Int
 import kotlin.Long
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.server.ResponseStatusException
 
 @Component("todo.TodoToDoHandler")
 class TodoToDoHandlerImpl : PassDatabaseHandler<TodoToDo, TodoToDoRepository>(),
@@ -19,9 +25,14 @@ class TodoToDoHandlerImpl : PassDatabaseHandler<TodoToDo, TodoToDoRepository>(),
      * Create ToDo: Creates a new ToDo object
      * HTTP Code 201: The created ToDo
      */
-    override suspend fun create(body: TodoToDo): TodoToDo {
-        TODO("not checked yet")
-        return repository.save(body)
+    override suspend fun create(body: TodoToDoDTO): TodoToDoDTO {
+        if (body.status !in Statuses.values().map { it.value }) {
+            throw ResponseStatusException(
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                "Invalid status: ${body.status}"
+            )
+        }
+        return repository.save(body.toEntity()).toDto()
     }
 
     /**
@@ -29,17 +40,16 @@ class TodoToDoHandlerImpl : PassDatabaseHandler<TodoToDo, TodoToDoRepository>(),
      * Headers will include TotalElements, TotalPages, CurrentPage and PerPage to help with Pagination.
      * HTTP Code 200: List of ToDos
      */
-    override suspend fun getAll(maxResults: Int = 100, page: Int = 0): Page<TodoToDo> {
-        TODO("not checked yet")
-        return repository.findAll(Pageable.unpaged())
+    override suspend fun getAll(maxResults: Int, page: Int): Page<TodoToDoDTO> {
+        val pagination = PageRequest.of(page, maxResults)
+        return repository.findAll(pagination).toDtos()
     }
 
     /**
      * Delete ToDo by id.: Deletes one specific ToDo.
      */
-    override suspend fun remove(id: Long): TodoToDo {
+    override suspend fun remove(id: Long) {
         val original = repository.findById(id).orElseThrow404(id)
-        TODO("not checked yet - update the values you really want updated")
         return repository.delete(original)
     }
 
@@ -49,9 +59,19 @@ class TodoToDoHandlerImpl : PassDatabaseHandler<TodoToDo, TodoToDoRepository>(),
      * HTTP Code 404: The requested object could not be found by the submitted id.
      * HTTP Code 422: On or many fields contains a invalid value.
      */
-    override suspend fun update(body: TodoToDo, id: Long): TodoToDo {
+    override suspend fun update(body: TodoToDoDTO, id: Long): TodoToDoDTO {
         val original = repository.findById(id).orElseThrow404(id)
-        TODO("not checked yet - update the values you really want updated")
-        return repository.save(original)
+
+        if (body.status != null && body.status !in Statuses.values().map { it.value }) {
+            throw ResponseStatusException(
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                "Invalid status: ${body.status}"
+            )
+        }
+
+        return repository.save(original.copy(
+            description = body.description ?: original.description,
+            status = body.status ?: original.description
+        )).toDto()
     }
 }
